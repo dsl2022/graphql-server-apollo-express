e5cad1e6c6e530bcd22758f2bb5d28a412d8b0d0
tmm <blocklocmedia@gmail.com>
2019-09-30 00:32:32 -0400

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,65 @@
-import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import schema from './schema';
 import resolvers from './resolvers';
-import models from './models';
-
-import cors from 'cors';
+import models, { sequelize } from './models';
 const app = express();
-
+app.use(cors());
 const server = new ApolloServer({
+	introspection: true,
+	playground: true,
 	typeDefs: schema,
 	resolvers,
-	context: {
+	context: async () => ({
 		models,
-		me: models.users[1]
-	}
+		me: await models.User.findByLogin('rwieruch')
+	})
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
-app.listen({ port: 8000 }, () => {
-	console.log('Apollo Server on http://localhost:8000/graphql');
+const eraseDatabaseOnSync = true;
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+	if (eraseDatabaseOnSync) {
+		createUsersWithMessages();
+	}
+	app.listen({ port: 8000 }, () => {
+		console.log('Apollo Server on http://localhost:8000/graphql');
+	});
 });
+
+const createUsersWithMessages = async () => {
+	await models.User.create(
+		{
+			username: 'rwieruch',
+			messages: [
+				{
+					text: 'Published the Road to learn React'
+				}
+			]
+		},
+		{
+			include: [ models.Message ]
+		}
+	);
+	await models.User.create(
+		{
+			username: 'ddavids',
+			messages: [
+				{
+					text: 'Happy to release ...'
+				},
+				{
+					text: 'Published a complete ...'
+				}
+			]
+		},
+		{
+			include: [ models.Message ]
+		}
+	);
+};
+
 // const userCredentials = { firstname: 'Robin' };
 // const userDetails = { nationality: 'German' };
 
